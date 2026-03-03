8d6eaadabace9eab88a76165380fa96a9b9e9677
f3-lab <kirermakov@icloud.com>
2026-03-03 02:11:05 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,27 @@
 import os
-import asyncio
-from telegram.ext import ApplicationBuilder
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = -1003891725312  # твой канал
 
-async def main():
-    print("STARTING BOT...")
+async def post(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🍽 Открыть меню", url="https://t.me/YOUR_BOT_USERNAME")]
+    ])
+
+    await context.bot.send_message(
+        chat_id=CHANNEL_ID,
+        text="🥟 F3 FOOD\nНажмите кнопку ниже, чтобы открыть меню:",
+        reply_markup=keyboard
+    )
+
+    await update.message.reply_text("Сообщение отправлено в канал")
+
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
-    print("BOT CREATED")
-    await app.run_polling()
+    app.add_handler(CommandHandler("post", post))
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
