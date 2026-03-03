345802d977be187f77ab4af42a823197a8f883a8
f3-lab <kirermakov@icloud.com>
2026-03-03 02:06:35 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,13 @@
 import os
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder
 import asyncio
+from telegram.ext import ApplicationBuilder
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = -1003891725312
 
 async def main():
+    print("STARTING BOT...")
     app = ApplicationBuilder().token(TOKEN).build()
-
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🍽 Открыть меню", url="https://t.me/@f3food_bot")]
-    ])
-
-    await app.bot.send_message(
-        chat_id=CHANNEL_ID,
-        text="🥟 F3 FOOD\nНажмите кнопку ниже, чтобы открыть меню:",
-        reply_markup=keyboard
-    )
-
+    print("BOT CREATED")
     await app.run_polling()
 
 if __name__ == "__main__":
